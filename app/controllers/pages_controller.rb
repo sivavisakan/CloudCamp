48c2ca8e613120ad0fdcdb2d4b1ee31a376fb015
Siva Visakan Sooriyan <sivavisakan@gmail.com>
2011-09-25 07:25:04 -0400

    Static Pages Added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def pittsburgh
+    @title = "Pittsburgh"
   end
 
   def register
+    @title = "Registration"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
